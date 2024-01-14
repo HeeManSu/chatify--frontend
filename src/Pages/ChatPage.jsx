a978bcbce72321069a830e83e34d6304662e60b1
Parth <parmarparth996@gmail.com>
2024-01-14 13:22:11 +0530

    added logic to home page

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
+import { getConversations } from "../redux/reducers/conversation-slice";
+import { useSelector } from "react-redux";
 
 const ChatPage = () => {
+  const { loading, conversations, error } = useSelector(
+    (state) => state.conversation
+  );
+  useEffect(() => {
+    getConversations();
+  }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
   return (
-    <div>
-      This is the chat page. 
-    </div>
-  )
-}
+    <>
+      {conversations.map((conversation) => {
+        return <div>{conversation.name}</div>;
+      })}
+      {conversations.length === 0 && <div>No conversations</div>}
+    </>
+  );
+};
 
-export default ChatPage
+export default ChatPage;
